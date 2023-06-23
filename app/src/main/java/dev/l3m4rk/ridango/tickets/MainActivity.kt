c8da901dd7bfe55c4efa43b5fa15c284494555cf
Dmitry <b4r4n0v@gmail.com>
2023-06-23 18:34:55 +0300

    ui: create screen UI for submitting product data

--- app/src/main/java/dev/l3m4rk/ridango/tickets/MainActivity.kt
+++ app/src/main/java/dev/l3m4rk/ridango/tickets/MainActivity.kt
@@ -4,28 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import dev.l3m4rk.ridango.tickets.ui.theme.TicketsTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject lateinit var text: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,9 +39,54 @@ class MainActivity : ComponentActivity() {
             TicketsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Text(text = text)
+                    BuyTicketScreen()
                 }
             }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BuyTicketScreen() {
+    var productName by remember { mutableStateOf("") }
+    var price by remember { mutableStateOf("") }
+
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = productName,
+            onValueChange = { productNameValue -> productName = productNameValue },
+            singleLine = true,
+            label = { Text(stringResource(R.string.label_product_name)) }
+        )
+        Spacer(Modifier.height(8.dp))
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = price,
+            onValueChange = { priceValue: String -> price = priceValue },
+            singleLine = true,
+            label = { Text(stringResource(R.string.label_price)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+        )
+        Spacer(Modifier.height(16.dp))
+        Button(
+            onClick = { /*TODO*/ }
+        ) {
+            Text(text = stringResource(R.string.button_buy_ticket))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BuyTicketScreenPreview() {
+    Surface {
+        BuyTicketScreen()
     }
 }
